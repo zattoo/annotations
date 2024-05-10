5c3ace2d6a8cb71f6c040bc11cacef2ac32f6186
sheethalgcin <98017227+sheethalgcin@users.noreply.github.com>
2024-05-10 07:47:21 +0200

    testing the release details

---
+++ Release/releaseInfo.py
@@ -0,0 +1,35 @@
+
+sheetal G.C <sheetalgcin@gmail.com>
+Thu, May 9, 8:23 PM (11 hours ago)
+to me
+
+import os
+import datetime
+import json
+import requests
+
+url = "https://events.zahs.tv/pt_annotations"
+annotations = {"events" : {},"v": 1}
+event = {"date": None, "component": "", "type": "", "tenant_id": "","version": ""}
+# Access environment variables
+release = os.environ.get('RELEASE_TAG')
+dateInfo = os.environ.get('RELEASE_DATE')
+datetime_obj = datetime.datetime.fromisoformat(dateInfo)
+# Convert datetime object to Unix epoch time
+epoch_time = int(datetime_obj.timestamp())
+
+event["date"] = int(epoch_time)
+event["component"] = "zolagus"
+event["type"]  = "release"
+event["tenant_id"] = 1
+event["version"] = str(release)
+annotations["events"]= event
+
+print(annotations)
+reponse = requests.post(url, json=annotations)
+
+if response.status_code == 200:
+        print("Data sent successfully to the endpoint.")
+else:
+  print("Failed to send data. Status code:", response.status_code)
+
