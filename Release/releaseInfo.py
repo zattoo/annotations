03afbfd404219bbacf7c48367de9bd83643a7daf
sheethalgcin <98017227+sheethalgcin@users.noreply.github.com>
2024-05-10 10:51:49 +0200

    correct the json response

--- Release/releaseInfo.py
+++ Release/releaseInfo.py
@@ -5,14 +5,14 @@ import json
 import requests
 
 url = "https://events.zahs.tv/pt_annotations"
-annotations = {"events" : {},"v": 1}
-event = {"date": None, "component": "", "type": "", "tenant_id": "","version": ""}
+event={"date": None, "component": "", "type": "", "tenant_id": 1, "version": ""}
+annotations = {"events": [{}], "v": 1}
 # Access environment variables
-release = os.environ.get('RELEASE_TAG')
-dateInfo = os.environ.get('RELEASE_DATE')
-datetime_obj = datetime.datetime.fromisoformat(dateInfo)
+ release = os.environ.get('RELEASE_TAG')
+ dateInfo = os.environ.get('RELEASE_DATE')
+ datetime_obj = datetime.datetime.fromisoformat(dateInfo)
 # Convert datetime object to Unix epoch time
-epoch_time = int(datetime_obj.timestamp())
+ epoch_time = int(datetime_obj.timestamp())
 
 event["date"] = int(epoch_time)
 event["component"] = "zolagus"
@@ -21,11 +21,11 @@ event["tenant_id"] = 1
 event["version"] = str(release)
 annotations["events"]= event
 
-print(annotations)
-response = requests.post(url, json=annotations)
+# Create a new dictionary with the desired format
+new_data = {"events": [annotations["events"]], "v": annotations["v"]}
+response = requests.post(url, json=new_data)
 
 if response.status_code == 200:
         print("Data sent successfully to the endpoint.")
 else:
-  print("Failed to send data. Status code:", response.status_code)
-
+        print("Failed to send data. Status code:", response.status_code)
