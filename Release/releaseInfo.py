f9b8aa71b2fc13009912adcc25450d7a42f7c73b
sheethalgcin <98017227+sheethalgcin@users.noreply.github.com>
2024-05-13 13:34:49 +0200

    Test release details

--- Release/releaseInfo.py
+++ Release/releaseInfo.py
@@ -27,6 +27,7 @@ def send_telemetry_annotation():
     # Endpoint URL for both staging and production
    # url_production = "https://events.zahs.tv/pt_annotations"
     url_staging  =  "https://events-staging.zahs.tv/pt_annotations"
+     event={"date": None, "component": "", "type": "", "tenant_id": None, "version": ""}
 
     # Environment variables
     dateInfo = os.environ.get('DATE')
@@ -54,37 +55,3 @@ def send_telemetry_annotation():
 
 if __name__ == "__main__":
     send_telemetry_annotation()
-
-
-
-
-
-import os
-import datetime
-import json
-import requests
-
-url = "https://events.zahs.tv/pt_annotations"
-event={"date": None, "component": "", "type": "", "tenant_id": 1, "version": ""}
-annotations = {"events": [{}], "v": 1}
-# Access environment variables
-dateInfo = os.environ.get('DATE')
-datetime_obj = datetime.datetime.fromisoformat(dateInfo)
-# Convert datetime object to Unix epoch time
-epoch_time = int(datetime_obj.timestamp())
-
-event["date"] = int(epoch_time)
-event["component"] = os.environ.get('COMPONENT')
-event["type"]  = os.environ.get('TYPE')
-event["tenant_id"] = int(os.environ.get('TENANT_ID'))
-event["version"] = str(os.environ.get('VERSION'))
-annotations["events"]= event
-
-   # Create annotations dictionary
-annotations = {"events": [event], "v": 1}
-response = requests.post(url, json=new_data)
-
-if response.status_code == 200:
-        print("Data sent successfully to the endpoint.")
-else:
-        print("Failed to send data. Status code:", response.status_code)
