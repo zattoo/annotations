f73c109aac66f8946e3fe0549d2382a4dc9ca7dc
sheethalgcin <98017227+sheethalgcin@users.noreply.github.com>
2024-05-15 10:32:42 +0200

    Test the annotation

--- TelemetryAnnotations/annotations.py
+++ TelemetryAnnotations/annotations.py
@@ -49,6 +49,5 @@ if __name__ == "__main__":
     # Endpoint URL for both staging and production
     URL_PRODUCTION = os.environ.get('urlProduction')
     URL_STAGING = os.environ.get('urlStaging')
-    print(URL_PRODUCTION,URL_STAGING)
     annotationToendpoints = TelemetryAnnotation(URL_PRODUCTION, URL_STAGING)
     annotationToendpoints.send_telemetry_annotation()
